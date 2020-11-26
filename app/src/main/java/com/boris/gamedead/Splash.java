dbee9bf353fba90fb4148114764f75a4d270fcce
dolave21 <75052730+dolave21@users.noreply.github.com>
2020-11-26 04:49:49 -0300

    Pantalla de carga

---
+++ app/src/main/java/com/boris/gamedead/Splash.java
@@ -0,0 +1,26 @@
+package com.boris.gamedead;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int DURACION_SPLASH = 3500;
+        //Handler ejecuta lineas de codigo en un tiempo determinado
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                //se ejecuta
+                Intent intent = new Intent(Splash.this,MainActivity.class);
+                startActivity(intent);
+            };
+        },DURACION_SPLASH);
+    }
+}
