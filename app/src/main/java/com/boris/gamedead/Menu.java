9ee40123963f8313fc80738e853aabc7a3c7769d
boris959 <boriss182@hotmail.com>
2020-11-28 17:27:03 -0300

    Cerrar sesion

--- app/src/main/java/com/boris/gamedead/Menu.java
+++ app/src/main/java/com/boris/gamedead/Menu.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,6 +16,8 @@ public class Menu extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser user;
 
+    Button CerrarSesion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,14 @@ public class Menu extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
+        CerrarSesion = findViewById(R.id.CerrarSesion);
+
+        CerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CerrarSesion();
+            }
+        });
     }
 
     @Override
@@ -38,6 +50,12 @@ public class Menu extends AppCompatActivity {
             startActivity(new Intent(Menu.this,MainActivity.class));
             finish();
         }
+    }
+
+    private void CerrarSesion(){
+        auth.signOut();
+        startActivity(new Intent(Menu.this,MainActivity.class));
+        Toast.makeText(this, "Cerrado sesion exitosamemte", Toast.LENGTH_SHORT).show();
     }
 
 
