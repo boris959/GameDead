466b00c2ef055509181b18d133f23fdc96920c87
boris959 <boriss182@hotmail.com>
2020-11-29 05:57:23 -0300

    escenario

--- app/src/main/java/com/boris/gamedead/Menu.java
+++ app/src/main/java/com/boris/gamedead/Menu.java
@@ -1,10 +1,12 @@
 package com.boris.gamedead;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,13 +14,26 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.SQLTransactionRollbackException;
+
 
 public class Menu extends AppCompatActivity {
 
     FirebaseAuth auth;
     FirebaseUser user;
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference JUGADORES;
+
     TextView MiPuntuaciontxt,uid,correo,nombre,Menu;
+    TextView Zombies;
     Button JugarBtn,PuntuacionesBtn,AcercaDeBtn,CerrarSesion;
 
     @Override
@@ -29,10 +44,15 @@ public class Menu extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        JUGADORES = firebaseDatabase.getReference("MI DATA BASE JUGADORES");
+
         String ubicacion = "fuentes/zombie.TTF";
         Typeface Tf = Typeface.createFromAsset(Menu.this.getAssets(),ubicacion);
 
         MiPuntuaciontxt = findViewById(R.id.MiPuntuaciontxt);
+
+        Zombies = findViewById(R.id.Zombies);
         uid = findViewById(R.id.uid);
         correo = findViewById(R.id.correo);
         nombre = findViewById(R.id.nombre);
@@ -59,6 +79,18 @@ public class Menu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(Menu.this, "JUGAR", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Menu.this, EscenarioJuego.class);
+
+                String UidS = uid.getText().toString();
+                String NombreS = nombre.getText().toString();
+                String ZombieS = Zombies.getText().toString();
+
+                intent.putExtra("UID",UidS);
+                intent.putExtra("NOMBRE",NombreS);
+                intent.putExtra("ZOMBIE",ZombieS);
+
+                startActivity(intent);
+                Toast.makeText(Menu.this, "ENVIANDO PARAMETROS", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -93,6 +125,7 @@ public class Menu extends AppCompatActivity {
     private void  UsuarioLogueado(){
 
         if (user != null){
+            Consulta();
             Toast.makeText(this, "Jugador en linea", Toast.LENGTH_SHORT).show();
         }
         else {
@@ -101,11 +134,40 @@ public class Menu extends AppCompatActivity {
         }
     }
 
+
+
     private void CerrarSesion(){
         auth.signOut();
         startActivity(new Intent(Menu.this,MainActivity.class));
         Toast.makeText(this, "Cerrado sesion exitosamemte", Toast.LENGTH_SHORT).show();
     }
 
+    private void Consulta(){
+        Query query = JUGADORES.orderByChild("Email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+
+                    String zombiesString = ""+ds.child("Zombies").getValue();
+                    String uidString = ""+ds.child("Uid").getValue();
+                    String emailString = ""+ds.child("Email").getValue();
+                    String nombreString = ""+ds.child("Nombres").getValue();
+
+                    Zombies.setText(zombiesString);
+                    uid.setText(uidString);
+                    correo.setText(emailString);
+                    nombre.setText(nombreString);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
 }
