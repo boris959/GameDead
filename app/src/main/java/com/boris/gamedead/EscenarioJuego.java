b4ff30bec3565bb99e3933e3908b7c0ef9a2b19b
dolave21 <75052730+dolave21@users.noreply.github.com>
2020-11-29 21:46:51 -0300

    Tamaño Pantalla + Mov Zombie + Contador

--- app/src/main/java/com/boris/gamedead/EscenarioJuego.java
+++ app/src/main/java/com/boris/gamedead/EscenarioJuego.java
@@ -2,17 +2,27 @@ package com.boris.gamedead;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.Display;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class EscenarioJuego extends AppCompatActivity {
 
     String UIDS, NOMBRES, ZOMBIES;
 
-    TextView TvContador,TvNombre,TvTiempo;
+    TextView TvContador,TvNombre,TvTiempo, AnchoTv, AltoTv;
     ImageView IvZombie;
+    int contador = 0;
+    int AnchoPantalla, AltoPantalla;
 
+    Random aleatorio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +33,9 @@ public class EscenarioJuego extends AppCompatActivity {
         TvContador = findViewById(R.id.TvContador);
         TvNombre = findViewById(R.id.TvNombre);
         TvTiempo = findViewById(R.id.TvTiempo);
+        AnchoTv = findViewById(R.id.AnchoTv);
+        AltoTv = findViewById(R.id.AltoTv);
+
 
         Bundle intent = getIntent().getExtras();
 
@@ -32,5 +45,61 @@ public class EscenarioJuego extends AppCompatActivity {
 
         TvNombre.setText(NOMBRES);
         TvContador.setText(ZOMBIES);
+        Pantalla();
+        CuentaAtras();
+        //al hacer click en la imagen del zombie
+        IvZombie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contador++;
+                TvContador.setText(String.valueOf(contador));
+                IvZombie.setImageResource(R.drawable.zombieaplastado);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        IvZombie.setImageResource(R.drawable.zombie2);
+                        Movimiento();
+                    }
+                }, 50);
+            }
+        });
     }
-}
+        private void Pantalla(){
+            Display display = getWindowManager().getDefaultDisplay();
+            Point point = new Point();
+            display.getSize(point);
+            AnchoPantalla = point.x;
+            AltoPantalla = point.y;
+            String ANCHOS = String.valueOf(AnchoPantalla);
+            String ALTOS = String.valueOf(AltoPantalla);
+
+            AnchoTv.setText(ANCHOS);
+            AltoTv.setText(ALTOS);
+            aleatorio = new Random();
+        }
+        private void Movimiento(){
+            int min =0;
+
+            int MaximoX = AnchoPantalla - IvZombie.getWidth();
+            int MaximoY = AltoPantalla - IvZombie.getHeight();
+            int randomX = aleatorio.nextInt(((MaximoX - min)+1)+min);
+            int randomY = aleatorio.nextInt(((MaximoY - min)+1)+min);
+            IvZombie.setX(randomX);
+            IvZombie.setY(randomY);
+
+        }
+        private void CuentaAtras(){
+            new CountDownTimer(30000, 1000) {
+
+                public void onTick (long millisUntilFinished) {
+                    long segundosRestantes = millisUntilFinished/1000;
+                    TvTiempo.setText(segundosRestantes+"S");
+                }
+
+                public void onFinish () {
+                    TvTiempo.setText ("OS");
+                }
+            }.start();
+        }
+    }
