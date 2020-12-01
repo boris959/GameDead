108ee4b56251991a57f88b9ef3eda5901ccd4259
boris959 <boriss182@hotmail.com>
2020-12-01 03:31:59 -0300

    editando diseño estetico

--- app/src/main/java/com/boris/gamedead/Menu.java
+++ app/src/main/java/com/boris/gamedead/Menu.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,7 +21,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
-import java.sql.SQLTransactionRollbackException;
+
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
